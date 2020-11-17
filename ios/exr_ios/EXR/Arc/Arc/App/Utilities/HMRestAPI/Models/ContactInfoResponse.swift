31ddb9e0a954adabab33d572fd856087a35ca17b
Michael Votaw <mike.votaw@thinkhappymedium.com>
2020-11-17 11:15:39 -0600

    Updating license information in ios projects.

--- ios/exr_ios/EXR/Arc/Arc/App/Utilities/HMRestAPI/Models/ContactInfoResponse.swift
+++ ios/exr_ios/EXR/Arc/Arc/App/Utilities/HMRestAPI/Models/ContactInfoResponse.swift
@@ -1,6 +1,13 @@
 //
 // ContactInfoResponse.swift
 //
+// This file is part of the Ambulatory Research in Cognition (ARC) Project. It is subject
+// to the license terms in the LICENSE file found in the top-level directory of this
+// distribution and at 
+// https://github.com/jasonhass/Ambulatory-Research-in-Cognition/blob/master/LICENSE
+// No part of this Project, including this file, may be copied, modified, propagated, or
+// distributed except according to the terms contained in the LICENSE file.
+
 
 
 
